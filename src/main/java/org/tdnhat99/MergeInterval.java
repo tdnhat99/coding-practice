dd9f9e49e3e19d775aa7b8d5931d4e054f9f38b8
Tran Dang Nhat <tdnhat99@gmail.com>
2025-12-15 00:21:41 +0700

    feat: merge interval

--- src/main/java/org/tdnhat99/MergeInterval.java
+++ src/main/java/org/tdnhat99/MergeInterval.java
@@ -1,5 +1,10 @@
 package org.tdnhat99;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/merge-intervals/description/
  * Merge Intervals (Medium)
@@ -9,6 +14,33 @@ package org.tdnhat99;
 public class MergeInterval {
 
     public int[][] merge(int[][] intervals) {
-        return null;
+        if (intervals == null || intervals.length <= 1) {
+            return intervals;
+        }
+
+        // Sort the intervals based on their start times
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
+        List<int[]> mergedIntervals = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        mergedIntervals.add(currentInterval);
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] nextInterval = intervals[i];
+            int currentEnd = currentInterval[1];
+            int nextStart = nextInterval[0];
+            int nextEnd = nextInterval[1];
+
+            if (currentEnd >= nextStart) {
+                // Overlapping interval, merge them
+                currentInterval[1] = Math.max(currentEnd, nextEnd);
+            } else {
+                // Non-overlapping interval, add it to the list
+                currentInterval = nextInterval;
+                mergedIntervals.add(currentInterval);
+            }
+        }
+
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
     }
 }
